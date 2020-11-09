28e366e8169776d29e1dbe8d488da909634b1de5
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-11-09 22:18:32 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,4 +1,12 @@
-def loop_message_five_times
+def loop_message_five_times (string)
+  #set the starting point
+  counter = 0 
+  
+  #this loops sets up our starting counter at 0, and sets the condition for the loop to run to be less than 5
+  while counter < 5 do
+    puts string
+    counter += 1 
+    #this sets the iteration and addition
 end
 
 def loop_message_n_times
