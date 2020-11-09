6cbbc457ba1f2a9d4d5dba49c21069055065ab03
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-11-09 22:42:32 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -29,10 +29,13 @@ end
 
 def return_string_array(array)
   counter = 0 
+  new_array = []
   
   while counter < array.length do 
-  array.to_s 
+    
+    new_array.push(array[counter].to_s)
+    counter += 1 
   end
-  new_array = array.to_s 
+  new_array
 end
 
