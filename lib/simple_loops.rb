defa01fbd3b9c47411345ed562955f0a6823f925
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-11-09 22:28:47 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -19,7 +19,13 @@ def loop_message_n_times (message, number)
   end
 end
 
-def output_array
+def output_array(array)
+  counter = 0 
+  
+  while array[counter] do 
+    puts array
+    counter += 1 
+  end 
 end
 
 def return_string_array
